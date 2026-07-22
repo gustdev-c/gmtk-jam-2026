07530195c6a85debb0c878a5a5554dff83b48c71
Gust <gustdevo@gmail.com>
2026-07-22 18:18:28 -0300

    Added the player character, used the basic movement template and added a double jump collectable using global variables

---
+++ double_jump.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@onready var character_body_2d: CharacterBody2D = $"../CharacterBody2D"
+@onready var area_2d: Area2D = $"."
+var id
+
+func _ready():
+	id = get_meta("powerup_id")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		match id:
+			1:
+				print("Double Jump")
+				global.maxJumps = 2
+			2:
+				print("Door")
+			3:
+				print("Weapon")
+		
+		queue_free()
---
+++ global_vars.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var maxJumps
+
+func _ready() -> void:
+	maxJumps = 1
