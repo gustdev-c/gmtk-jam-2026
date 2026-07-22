c9db8d94fb5230314fd63ac43e13d29398cb5f87
Gust <gustdevo@gmail.com>
2026-07-22 19:44:48 -0300

    Hacky implementation of the "Cooldown" theme

--- double_jump.gd
+++ double_jump.gd
@@ -9,14 +9,19 @@ func _ready():
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "CharacterBody2D":
+		area_2d.visible = false
 		match id:
 			1:
 				print("Double Jump")
 				global.maxJumps = 2
 				global.canDoubleJump = true
+				await get_tree().create_timer(5.0).timeout
+				print("Double jump over")
+				global.maxJumps = 1
+				global.canDoubleJump = false
+				queue_free()
 			2:
 				print("Door")
 			3:
 				print("Weapon")
 		
-		queue_free()
